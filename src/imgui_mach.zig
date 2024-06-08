d5790495da5ec0282aa65e7eecf541f951afb44f
foxnne <foxnne@gmail.com>
2024-06-08 02:15:02 -0500

    imgui_mach: Fix compile error

--- src/imgui_mach.zig
+++ src/imgui_mach.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const imgui = @import("imgui.zig");
 const core = @import("mach").core;
-const gpu = core.gpu;
+const gpu = @import("mach").gpu;
 
 var allocator: std.mem.Allocator = undefined;
 
