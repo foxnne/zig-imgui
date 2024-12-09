d978154d072d6e64a3bfaaa311673e4260a358f0
foxnne <foxnne@gmail.com>
2024-12-09 07:26:50 -0600

    update to latest mach

--- examples/example_mach.zig
+++ examples/example_mach.zig
@@ -3,26 +3,52 @@ const build_options = @import("build-options");
 const imgui = @import("imgui");
 const imgui_mach = imgui.backends.mach;
 const mach = @import("mach");
-const core = mach.core;
+const Core = mach.Core;
 const gpu = mach.gpu;
 
 pub const App = @This();
 
+pub const mach_module = .app;
+
+pub const mach_systems = .{ .main, .init, .tick, .deinit };
+
+pub const main = mach.schedule(.{
+    .{ mach.Core, .init },
+    .{ App, .init },
+    .{ mach.Core, .main },
+});
+
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var allocator: std.mem.Allocator = undefined;
 
-title_timer: core.Timer,
+window: mach.ObjectID,
+title_timer: mach.time.Timer,
 f: f32 = 0.0,
 color: [3]f32 = undefined,
 
-pub fn init(app: *App) !void {
-    try core.init(.{});
-
+pub fn init(app: *App, core: *Core, app_mod: mach.Mod(App)) !void {
+    core.on_tick = app_mod.id.tick;
+    core.on_exit = app_mod.id.deinit;
     allocator = gpa.allocator();
+
+    const window = try core.windows.new(.{
+        .title = "ImGui",
+        .vsync_mode = .double,
+    });
+
+    app.* = .{
+        .title_timer = try mach.time.Timer.start(),
+        .window = window,
+    };
+}
+
+pub fn lateInit(app: *App, core: *Core) !void {
+    const window = core.windows.getValue(app.window);
 
     imgui.setZigAllocator(&allocator);
     _ = imgui.createContext(null);
-    try imgui_mach.init(allocator, core.device, .{});
+
+    try imgui_mach.init(core, allocator, window.device, .{ .color_format = window.framebuffer_format });
 
     var io = imgui.getIO();
     io.config_flags |= imgui.ConfigFlags_NavEnableKeyboard;
@@ -40,47 +66,43 @@ pub fn init(app: *App) !void {
     font_cfg.rasterizer_density = 1.0;
     font_cfg.ellipsis_char = imgui.UNICODE_CODEPOINT_MAX;
     _ = io.fonts.?.addFontFromMemoryTTF(@constCast(@ptrCast(font_data.ptr)), font_data.len, size_pixels, &font_cfg, null);
-
-    app.* = .{
-        .title_timer = try core.Timer.start(),
-    };
 }
 
 pub fn deinit(app: *App) void {
     _ = app;
     defer _ = gpa.deinit();
-    defer core.deinit();
 
     imgui_mach.shutdown();
     imgui.destroyContext(null);
 }
 
-pub fn update(app: *App) !bool {
-    var iter = core.pollEvents();
-    while (iter.next()) |event| {
-        _ = imgui_mach.processEvent(event);
-
+pub fn tick(app: *App, core: *Core) !void {
+    while (core.nextEvent()) |event| {
         switch (event) {
-            .close => return true,
+            .window_open => {
+                try lateInit(app, core);
+            },
+            .close => {},
             else => {},
         }
+        _ = imgui_mach.processEvent(event);
     }
 
-    try app.render();
+    try render(app, core);
 
     // update the window title every second
-    if (app.title_timer.read() >= 1.0) {
-        app.title_timer.reset();
-        try core.printTitle("ImGui [ {d}fps ] [ Input {d}hz ]", .{
-            core.frameRate(),
-            core.inputRate(),
-        });
-    }
-
-    return false;
+    // if (app.title_timer.read() >= 1.0) {
+    //     app.title_timer.reset();
+    //     try core.printTitle("ImGui [ {d}fps ] [ Input {d}hz ]", .{
+    //         core.frameRate(),
+    //         core.inputRate(),
+    //     });
+    // }
 }
 
-fn render(app: *App) !void {
+fn render(app: *App, core: *Core) !void {
+    const window = core.windows.getValue(app.window);
+
     const io = imgui.getIO();
 
     imgui_mach.newFrame() catch return;
@@ -94,7 +116,7 @@ fn render(app: *App) !void {
 
     imgui.render();
 
-    const back_buffer_view = core.swap_chain.getCurrentTextureView().?;
+    const back_buffer_view = window.swap_chain.getCurrentTextureView().?;
     const color_attachment = gpu.RenderPassColorAttachment{
         .view = back_buffer_view,
         .clear_value = gpu.Color{ .r = 0.2, .g = 0.2, .b = 0.2, .a = 1.0 },
@@ -102,7 +124,7 @@ fn render(app: *App) !void {
         .store_op = .store,
     };
 
-    const encoder = core.device.createCommandEncoder(null);
+    const encoder = window.device.createCommandEncoder(null);
     const render_pass_info = gpu.RenderPassDescriptor.init(.{
         .color_attachments = &.{color_attachment},
     });
@@ -115,10 +137,10 @@ fn render(app: *App) !void {
     var command = encoder.finish(null);
     encoder.release();
 
-    var queue = core.queue;
+    var queue = window.queue;
     queue.submit(&[_]*gpu.CommandBuffer{command});
     command.release();
-    core.swap_chain.present();
+    window.swap_chain.present();
     back_buffer_view.release();
 }
 
